be9c342bb4a62bfc140d55d2e5fc98528923214b
anfnamazi <anf.namazi@gmail.com>
2019-12-13 07:51:15 -0800

    add effect moveUpIcon & ...

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 $(".button-collapse").sideNav();
+
+$(".collapsible").click(function () {
+    if ($(".collapsible-header").hasClass("active")) {
+        $(".collapsible-icon").addClass("move-up-icon");
+    } else {
+        $(".collapsible-icon").removeClass("move-up-icon");
+    }
+});
